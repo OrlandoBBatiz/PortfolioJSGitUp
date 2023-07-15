fafe21114b61892d4a333419a75ab4e65e24d8b0
Orlando Batiz <72784310+OrlandoBBatiz@users.noreply.github.com>
2023-07-15 16:41:27 +0000

    Mis primeros cambios en el Portafolio del Pana Orlando Batiz para CodeSpace

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "",
+  title: "",
+  email: "b.orlando.batiz@gmail.com",
+  gitHub: "orlandobbatiz",
+  instagram: "orlando_batiz",
+  linkedIn: "orlandobatiz",
+  twitter: "OrlandoBatiz",
+  youTube: "OrlandoBatiz",
 };
 
 const primaryColor = "#4E567E";
